780b50266dc7b3ebaadd42ebd17fcfe3df85d3fa
m16010133 <vincent.mazel@etu.univ-amu.fr>
2018-02-22 11:31:24 +0100

    une réussite totale

--- json/connexion.php
+++ json/connexion.php
@@ -4,5 +4,3 @@ session_start();
 $_SESSION["username"] = $_POST["username"];
 $_SESSION["password"] = $_POST["password"];
 
-
-header("location:/index.html");
--- json/est_connectee.php
+++ json/est_connectee.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-$isConnecte = new stdClass();
-$isConnecte = false;
+$resultat = new stdClass();
+$resultat->isConnecte = false;
 if (isset($_SESSION["username"]))
-    $isConnecte = true;
+    $resultat->isConnecte = true;
 
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
-echo json_encode($isConnecte);
+echo json_encode($resultat);
 
